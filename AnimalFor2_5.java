193eb5b148b13a690ebdfab05a96bb5cccc45cee
Rey <reikanod@yandex.ru>
2025-05-23 15:10:28 +0300

    afdsa

--- AnimalFor2_5.java
+++
@@ -1,9 +0,0 @@
-public abstract class AnimalFor2_5 {
-    public int limSwim;
-    public int limRun;
-    public static int countDogs = 0;
-    public static int countCats = 0;
-
-    public abstract void run(int len) throws Exception;
-    public abstract void swim(int len);
-}
