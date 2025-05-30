c87362ef42b183df8d949a920eda5226e3693c59
Rey <reikanod@yandex.ru>
2025-05-30 19:01:58 +0300

    commit

--- Start.java
+++ Start.java
@@ -1,20 +1,35 @@
+import java.util.*;
+
 public class Start {
     public static void main(String[] args) {
-        // Вывожу в консоль данные о продукте
-        System.out.println("Вывожу первую машину");
-        Product gorgeousCar = new Product("Lada Niva", "2024", "VAZ", "Russia", 2000000, false);
-        gorgeousCar.displayProduct();
+        // Заполнение тестовых данных
+        ArrayList<String> last_names= new ArrayList<>(List.of(
+                "Игогов",
+                "Сидров",
+                "Сидров",
+                "Пивов",
+                "Китов"
+        ));
+        ArrayList<String> phones= new ArrayList<>(List.of(
+                "+7789456123",
+                "+8789456123",
+                "+9789456123",
+                "+6789456123"
+        ));
 
-        // Создаем массив из 5 товаров и проверяю вывод
-        System.out.println("\nВывожу список из 5 продуктов");
-        Product[] productsArray = new Product[5];
-        productsArray[0] = new Product("Sasung S105", "2025", "PodvalProduction", "Uzbekistan", 2000, true);
-        productsArray[1] = new Product("Abibas 2poi", "2025", "PodvalProduction", "Uzbekistan", 2000, true);
-        productsArray[2] = new Product("Niketos 4you", "2025", "PodvalProduction", "Uzbekistan", 2000, true);
-        productsArray[3] = new Product("Appil 8me", "2025", "PodvalProduction", "Uzbekistan", 2000, true);
-        productsArray[4] = new Product("XiXiomi touchMe", "2025", "PodvalProduction", "Uzbekistan", 2000, true);
-        for (Product product : productsArray) {
-            product.displayProduct();
+        Random rand = new Random();
+        for (String name : last_names) {
+            Person person = new Person(name, phones.get(rand.nextInt(phones.size())));
+            TeleBook.addPerson(person);
         }
+
+        // Получаем всех людей в справочнике
+        System.out.println("Вся книга");
+        TeleBook.printPeople(TeleBook.getBook());
+        // Получаем человека с конкретной фамилией
+
+        String lastName = "Сидров";
+        System.out.println("\nЛюди с фамилией " + lastName);
+        TeleBook.printPeople(TeleBook.getPersons(lastName));
     }
 }
