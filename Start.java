9e0774902dade279c320d961415d2176e4bd2175
Rey <reikanod@yandex.ru>
2025-05-30 14:15:43 +0300

    afdsa

--- Start.java
+++ Start.java
@@ -2,21 +2,7 @@ import java.util.*;
 
 public class Start {
     public static void main(String[] args) {
-        ArrayList<Student> students = new ArrayList<>();
-
         // тестовые данные
-        ArrayList<String> courses = new ArrayList<>(List.of(
-                "Зельеварение",
-                "Магловедение",
-                "Мать и Матиха",
-                "Живноведение",
-                "Растенелогия",
-                "Гусский язык",
-                "Землелогия",
-                "Здравоведение"
-        ));
-
-
         ArrayList<String> names = new ArrayList<>(List.of(
                 "Умнолоб",
                 "Мордоклюв",
@@ -32,31 +18,59 @@ public class Start {
                 "Штанголюб",
                 "Котоглад"
         ));
-        ArrayList<String> groups = new ArrayList<>(List.of(
-                "Пыхтуны",
-                "Спуны",
-                "Добруны",
-                "Силуны"
-        ));
 
-        // создание списка студентов и заполнение тестовыми данными
+        // Создание списка студентов и заполнение тестовыми данными
         Random random = new Random();
         int indexName;
         while (!names.isEmpty()) {
             indexName = random.nextInt(names.size());
-            Student student = new Student(names.get(indexName), groups.get(random.nextInt(groups.size())), courses);
+            Student student = new Student(
+                    names.get(indexName),
+                    Hogwarts.getGroups().get(random.nextInt(Hogwarts.getGroups().size())),
+                    Hogwarts.getCourses(),
+                    Hogwarts.getEducationYears().get(random.nextInt(Hogwarts.getEducationYears().size()))
+            );
             names.remove(indexName);
-            for (String course : courses) {
+
+            for (String course : student.getCourses()) {
                 student.setMark(course, random.nextInt(5) + 1);
             }
-            students.add(student);
+            Hogwarts.addStudent(student);
         }
 
-        // проверяю вывод оценок и заполненность данных всех студентов
-        for (Student student : students) {
-            student.getMarks();
+        // проверяю заполненность данных студентов. Одного достаточно
+        for (Student student : Hogwarts.getStudents()) {
+            student.printMarks();
+            break;
         }
 
+        // Проверяю добавление студента
+        System.out.println("\nВсего студентов: " + Hogwarts.getStudents().size());
+        Student student = new Student(
+                "Магослав",
+                Hogwarts.getGroups().get(random.nextInt(Hogwarts.getGroups().size())),
+                Hogwarts.getCourses(),
+                4
+                );
+        Hogwarts.addStudent(student);
+        System.out.println("Добавили " + student.getName() + "a\n" + "Всего студентов: " + Hogwarts.getStudents().size());
 
+        // Проверяю отчисления
+        System.out.println("\nВсего студентов: " + Hogwarts.getStudents().size());
+        Hogwarts.excludeStupid();
+        System.out.println("Удалили глупышей");
+        System.out.println("Всего студентов: " + Hogwarts.getStudents().size());
+
+        // Проверяю метод перевода на следующий курс
+        Student stud =  Hogwarts.getStudents().get(random.nextInt(Hogwarts.getStudents().size()));
+        System.out.println("\nСлучайно выбран: " + stud.getName());
+        System.out.println("Его курс: " + stud.getEducationYear());
+        System.out.println("Переводим его на следующий курс");
+        Hogwarts.transferStudentNextYear(stud);
+        System.out.println("Теперь его курс: " + stud.getEducationYear());
+
+        // Выводим всех студентов и их курс
+        System.out.println("\nВсе студенты:");
+        Hogwarts.printStudents();
     }
 }
--- Student.java
+++ Student.java
@@ -1,34 +1,80 @@
 import java.util.*;
 
 public class Student {
+    private static HashSet<Integer> studentIdList = new HashSet<>(); // занятые стуентами id
+    private int studentID; // id студента
     private String name;
     private String group;
     private ArrayList<String> courses;
     private HashMap<String, Integer> marks;
+    private int educationYear;
 
-
-    public Student(String name, String group, ArrayList<String> courses) {
+    // Конструктор
+    public Student(String name, String group, ArrayList<String> courses, int edYear) {
         this.name = name;
         this.group = group;
         this.courses = courses;
         this.marks = new HashMap<>();
+        this.educationYear = edYear;
         for (String course : courses) {
             this.marks.put(course, 0);
         }
+
+        // Находим свободный ID, добавляем в studentIdList его айдишник и присваиваем его студенту
+        for (int i = 1; i <= studentIdList.size() + 2; i++) {
+            if (studentIdList.contains(i)) {
+                continue;
+            } else {
+                studentIdList.add(i);
+                this.studentID = i;
+                break;
+            }
+        }
     }
 
+    // Геттеры
+    public int getEducationYear() {
+        return this.educationYear;
+    }
+    public int getStudentID() {
+        return this.studentID;
+    }
     public HashMap<String, Integer> getMarks() {
-        System.out.println("\nОценки " + this.name + "a:");
-        for (Map.Entry<String, Integer> entry : this.marks.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
         return this.marks;
+    }
+    public String getName() {
+        return  this.name;
+    }
+    public ArrayList<String> getCourses() {
+        return this.courses;
+    }
+
+    // Сеттеры
+    public void setEducationYear(int year) {
+        this.educationYear = year;
     }
     public void setMark(String course, int mark) {
         for (Map.Entry<String, Integer> entry : this.marks.entrySet()) {
             if (Objects.equals(entry.getKey(), course)) {
                 entry.setValue(mark);
             }
+        }
+    }
+
+    // Остальные
+    // Получить средний балл студента
+    public double avrGrade() {
+        double sum = 0;
+        for (Integer mark: this.getMarks().values()) {
+            sum += mark;
+        }
+        return sum / this.getMarks().size();
+    }
+    // Распечатать оценки
+    public void printMarks() {
+        System.out.println("\nОценки " + this.name + "a:");
+        for (Map.Entry<String, Integer> entry : this.marks.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
